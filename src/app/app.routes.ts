4d1c1cab3d2078c94ba1a921538a1d9dfee1b75e
khaled <khalednaguib55555@gmail.com>
2024-07-16 14:59:40 +0300

    gtr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import path from 'node:path';
+import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { ProductsDetailsComponent } from './products/components/products-details/products-details.component';
+import { CartComponent } from './carts/components/cart/cart.component';
+export const routes: Routes = [
+  { path: 'products', component: AllProductsComponent }, 
+  { path: 'details', component: ProductsDetailsComponent },
+   { path: 'cart', component:CartComponent  },
+    { path: '**', redirectTo: '/products',pathMatch:"full" }
+];
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    CommonModule,RouterModule,HttpClient
+  ],
+  exports:[HeaderComponent]
+})
+export class SharedModule { }
